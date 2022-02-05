d0512c973aed74248d2ecb5208bea832bca11643
iAe <kaduk_kadic@live.com>
2022-02-05 16:22:08 +0700

    Change Name

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'iAe'
+const name = 'iAeZz learning aboub NextJs'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
